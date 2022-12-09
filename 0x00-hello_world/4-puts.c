d99bb7cb01cdf113cf03c4ebafdfa39515aecc35
Prince Narteh <prinarteh@gmail.com>
2022-12-08 18:19:28 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdin.h>
+
+/**
+  * main - prints string using put function
+  * Return: 0
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
