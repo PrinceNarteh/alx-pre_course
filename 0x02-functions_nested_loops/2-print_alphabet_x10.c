8daf29bd37bef2143d08049129eae44f858c4e76
Prince Narteh <prinarteh@gmail.com>
2022-12-14 19:06:58 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - function that will print the alphabet
+ *
+*/
+
+void print_alphabet_x10(void)
+{
+	int line;
+	char ch = 'a';
+
+	for (line = 0; line < 10; line++)
+	{
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+	}
+}
