6b19480029e8da8e355dff4f94c4a8c6600fd741
Prince Narteh <prinarteh@gmail.com>
2022-12-15 02:34:09 -0800

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * print_most_numbers - print 0 - 9 apart
+ *                 from 2 and 4 and you can
+ *                 only use _putchar twice
+ * Return: Always 0 (Success)
+*/
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 48; num <= 58; num++)
+	{
+		if (num == 2 || num == 4)
+			continue;
+		_putchar(num);
+	}
+	_putchar('\n');
+}
