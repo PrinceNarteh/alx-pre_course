dfb8c5001e45485266c8d81d2b9dd8895716e055
Prince Narteh <prinarteh@gmail.com>
2022-12-19 06:11:03 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+/**
+  * swap_int - swaps to integers
+  * @a: first integer
+  * @b: second integer
+  *
+  * Return: void
+  */
+void swap_int(int *a, int *b)
+{
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
